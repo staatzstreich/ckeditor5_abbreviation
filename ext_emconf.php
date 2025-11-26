5e972cf364b9a6e124a24f61ce62b268076f8503
michael staatz <michael@staatzstreich.de>
2025-11-26 19:06:01 +0100

    [TASK] Add support for TYPO3 v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,11 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'author' => 'Michael Staatz',
     'author_email' => 'kontakt@mstaatz.de',
-    'version' => '1.1.4',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
-            'rte_ckeditor' => '13.4.0-13.4.99'
+            'typo3' => '13.4.0-14.3.99',
+            'rte_ckeditor' => '13.4.0-14.3.99'
         ],
     ],
 ];
